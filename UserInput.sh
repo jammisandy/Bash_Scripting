77271360f8bd1bc3004b7fefdec3c9f99c6a1a42
jammisandy <109514629+jammisandy@users.noreply.github.com>
2024-02-06 11:11:48 -0600

    Create UserInput.sh

---
+++ UserInput.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "The script is from User input"
+echo "Enter your skills"
+read SKILLS
+echo "$SKILLS are high on demand"
+
+read -p 'Username: ' USR
+read -sp 'Password: ' pass
+
+echo
+echo "Login successful:Welcome USER $USR."
